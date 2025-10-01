120dd07a08404fb36c782158041c0bd8e429999c
Kutbuddin814 <kutbuddinshaikh814@gmail.com>
2025-10-01 07:04:06 +0530

    Delete full stack project directory

--- full stack project/backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("freelancer_erp", "root", "root123", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
-
-module.exports = sequelize;
